6df2053c116f68b9ac2e3c4be223d7488e2d3cdd
FM-PC-LT-132 <fm-pc-lt-132@FM-PC-LT-132s-MacBook-Pro.local>
2019-11-12 12:28:40 +0545

    [Home Activity] Setup Home Activity with Drawer Layout 	- Home Activity Setup done with Navigation Graph. 	- Added Drawer Layout to switch between fragments.

--- app/src/main/java/com/rubin/mvvmdemo/ui/home/HomeActivity.kt
+++ app/src/main/java/com/rubin/mvvmdemo/ui/home/HomeActivity.kt
@@ -2,13 +2,33 @@ package com.rubin.mvvmdemo.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.rubin.mvvmdemo.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_home)
+
+        init()
+    }
+
+    private fun init() {
+        setSupportActionBar(toolbar)
+
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawer_layout)
     }
 }
